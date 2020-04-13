cafe8fee8478cdf61811a7bf885274ee39acca61
ashyrokoriadov <andrey.sh@wp.pl>
2020-04-13 20:14:15 +0200

    Added switch and its unit tests

---
+++ Calculator/Abstractions/Conditions/IOrderedCondition.cs
@@ -0,0 +1,7 @@
+﻿namespace Calculator.Abstractions.Conditions
+{
+    public interface IOrderedCondition<out TOrderBy, in T> : ICondition<T>
+    {
+        TOrderBy Order { get; }
+    }
+}
---
+++ Calculator/Switches/Switch.cs
@@ -0,0 +1,34 @@
+﻿
+using System.Collections.Generic;
+using System.Linq;
+using Calculator.Abstractions.Conditions;
+using Calculator.Abstractions.Switch;
+using Calculator.Models;
+
+namespace Calculator.Switches
+{
+    public class Switch<TOrderValue> : ISwitch<CalculatorValue>
+    {
+        public Switch(IReadOnlyDictionary<IOrderedCondition<TOrderValue, CalculatorValue>, decimal> conditions, decimal defaultValue)
+        {
+            var orderedDictionary
+                = conditions.OrderBy(x => x.Key.Order)
+                    .ToDictionary(x=> x.Key, x => x.Value);
+
+            Conditions = orderedDictionary;
+            DefaultValue = defaultValue;
+        }
+
+        public decimal Evaluate(CalculatorValue comparingValue)
+        {
+            var conditionKey = Conditions.Keys
+                .FirstOrDefault(value => value.Evaluate(comparingValue));
+
+            return conditionKey == null ? DefaultValue : Conditions[conditionKey];
+        }
+
+        public IReadOnlyDictionary<IOrderedCondition<TOrderValue, CalculatorValue>, decimal> Conditions { get; }
+
+        public decimal DefaultValue { get; }
+    }
+}
