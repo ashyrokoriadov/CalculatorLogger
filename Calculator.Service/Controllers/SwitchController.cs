34e9bffbf5e9fae904c9def9689e2afec9329aac
ashyrokoriadov <andrey.sh@wp.pl>
2020-04-23 18:52:14 +0200

    Added validator to Switch Controller

--- Calculator.Service/Controllers/SwitchController.cs
+++ Calculator.Service/Controllers/SwitchController.cs
@@ -28,6 +28,16 @@ namespace Calculator.Service.Controllers
         [HttpGet]
         public ActionResult<CalculatorValue> ResolveSwitch([FromBody] SwitchPayload payload)
         {
+            if (payload == null)
+                return BadRequest($"{nameof(ConditionPayload)} cannot be null.");
+
+            if (payload.ReferenceData == null || !payload.ReferenceData.Any())
+                return BadRequest($"{nameof(payload.ReferenceData)} cannot be null or empty.");
+
+            if (_nullValidator.Validate(payload.ComparingValue))
+                return BadRequest($"{nameof(payload.ComparingValue)} cannot be null.");
+
+
             var switchData = GetSwitchData(payload);
 
             ISwitch<CalculatorValue> resolver  = new Switch<int>(switchData, payload.DefaultValue);
@@ -36,13 +46,17 @@ namespace Calculator.Service.Controllers
             return Ok(operationResult);
         }
 
-        private static Dictionary<IOrderedCondition<int, CalculatorValue>, decimal> GetSwitchData(SwitchPayload payload)
+        private Dictionary<IOrderedCondition<int, CalculatorValue>, decimal> GetSwitchData(SwitchPayload payload)
         {
             var result = new Dictionary<IOrderedCondition<int, CalculatorValue>, decimal>();
 
             foreach (var item in payload.ReferenceData)
             {
+                if (item.Conditions == null || !item.Conditions.Any())
+                    continue;
+
                 var conditions = item.Conditions
+                    .Where(condition => condition?.ReferenceValue != null)
                     .Select(mConditionItem => new Condition(mConditionItem.ReferenceValue, mConditionItem.Operator))
                     .Cast<ICondition<CalculatorValue>>();
 
